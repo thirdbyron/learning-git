7b26ee989df8d88b5960350f633b171d9ad848b3
thirdbyron <rgnatovsky@gmail.com>
2022-08-08 11:20:37 +0300

    fix: js-test to fixed

--- js-test.js
+++ js-test.js
@@ -1,22 +1,13 @@
-const returnLastObject = (keysSequence, objectVariety) => {
+const returnLastObject = (keysSequence, nestedObject) => {
   const keysArray = keysSequence.split(".");
-  let tempObj = objectVariety;
+  let tempObj = nestedObject;
   for (key of keysArray) {
     if (tempObj[key]) {
       tempObj = tempObj[key];
     } else {
-      console.log(undefined)
       return undefined;
     }
-
   }
-  console.log(tempObj)
   return tempObj;
 }
 
-returnLastObject("hallo.hey.privet.ego", {hallo: {
-  hey: {
-    c: {},
-    privet: []
-  }
-}})
